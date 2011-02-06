ca2149a1f7defb47201db6abbac934a811c8f23b
Brandon Montgomery <bmonty@gmail.com>
2011-02-06 09:12:35 -0500

    fix soem problems with matrix calculations

--- bemdiff.pl
+++ bemdiff.pl
@@ -13,36 +13,62 @@ $rowCount = length($newString) + 1;
 $editMatrix = new Matrix($colCount, $rowCount);
 
 # initialize distances for first column
-for ($i = 0; $i < $rowCount; $i++) { 
-  $editMatrix->setValue(0, $i, new EditMatrixNode($i, "fromtop")); 
+for ($i = 1; $i <= $rowCount; $i++) { 
+  #print "x: 0, y: $i, dis: $i fromtop\n";
+  $editMatrix->setValue(1, $i, new EditMatrixNode($i, "fromtop")); 
 }
 
 # initialize distances for first row
-for ($i = 0; $i < $colCount; $i++) { 
-  $editMatrix->setValue($i, 0, new EditMatrixNode($i, "fromleft")); 
+for ($i = 1; $i <= $colCount; $i++) { 
+  #print "x: $i, y: 0, dis: $i fromleft\n";
+  $editMatrix->setValue($i, 1, new EditMatrixNode($i, "fromleft")); 
 }
 
-#$editMatrix->print;
+#$editMatrix->print("getVector");
 
-#now, start at 1,1 and determine the best way to get there
-for ($x = 1; $x < $colCount; $x++) {
-  for ($y = 1; $y < $rowCount; $y++) {
+#now, start at 2,2 and determine the best way to get there
+for ($x = 2; $x <= $colCount; $x++) {
+  for ($y = 2; $y <= $rowCount; $y++) {
     #first, see if it's a diagonal
     if (substr($oldString, $x - 1, 1) eq substr($newString, $y - 1, 1)) {
       $editMatrix->setValue($x, $y, new EditMatrixNode($editMatrix->getValue($x - 1, $y - 1)->{ _distance }, "diag"));
+      #print "x: $x, y: $y, dir: diag\n";
     } else {
       #try from the top and from the left, see which is better.
-      $distanceFromTop = $editMatrix->getValue($x, $y - 1)->{ _distance } + 1;
-      $distanceFromLeft = $editMatrix->getValue($x - 1, $y)->{ _distance } + 1;
+      $distanceFromTop = $editMatrix->getValue($x, $y - 1)->getDistance() + 1;
+      $distanceFromLeft = $editMatrix->getValue($x - 1, $y)->getDistance() + 1;
       if ($distanceFromTop < $distanceFromLeft) {
         #top looks good! we'll take it.
         $editMatrix->setValue($x, $y, new EditMatrixNode($distanceFromTop, "fromtop"));
+        #print "x: $x, y: $y, dir: fromtop; dl: $distanceFromLeft, dt: $distanceFromTop\n";
       } else {
         #left looks better! we'll take that instead.
         $editMatrix->setValue($x, $y, new EditMatrixNode($distanceFromLeft, "fromleft"));
+        #print "x: $x, y: $y, dir: fromleft; dl: $distanceFromLeft, dt: $distanceFromTop\n";
       }
     }
   }
 }
 
 $editMatrix->print("getVector");
+
+#transform the edit matrix into the minimum list of steps to transform the old string into the new string
+#start at the bottom-right node, and move backwards through the edit matrix, following the recorded directions
+$x = $colCount - 1;
+$y = $rowCount - 1;
+@steps = ();
+while($x != 1 || $y != 1) {
+  #record direction - only need to record it if it's not diagonal
+  $dir = $editMatrix->getValue($x, $y)->getDirection();
+  #print "x: $x, y: $y; dir: $dir\n";
+  if (!($dir eq "d")) {
+    unshift(@steps, $dir);
+  }
+  $x -= $dir eq "d" || $dir eq "l" ? 1 : 0;
+  $y -= $dir eq "d" || $dir eq "t" ? 1 : 0;
+}
+
+#print out the steps
+foreach (@steps) {
+  print "$_\n";
+}
--- matrix.pl
+++ matrix.pl
@@ -15,10 +15,8 @@ sub getValue {
   $self = shift;
   $x = shift;
   $y = shift;
-  $xDim = $self->{ _xDim };
-  $yDim = $self->{ _yDim };
   if (defined($self->{ _matrixArray })) {
-    return $self->{ _matrixArray }[$x + $y * $self->{ _yDim }];
+    return $self->{ _matrixArray }[$self->getMatrixIndex($x, $y)];
   } else {
     return 0;
   }
@@ -30,22 +28,32 @@ sub setValue {
   $x = shift;
   $y = shift;
   $value = shift;
-  $xDim = $self->{ _xDim };
-  $yDim = $self->{ _yDim };
-  $self->{ _matrixArray }[$x + $y * $self->{ _yDim }] = $value;
+  $self->{ _matrixArray }[$self->getMatrixIndex($x, $y)] = $value;
 }
 
 # this method prints out the matrix in an easy to read format. it allows for a parameter which tells what method to run during the print. mostly for debugging purposes.
 sub print {
   $self = shift;
   $method = shift;
-  for ($r = 0; $r < $self->{ _yDim }; $r++) {
-    for ($c = 0; $c < $self->{ _xDim }; $c++) {
-      $printVal = $self->{ _matrixArray }[$c + $r * $self->{ _yDim }]->$method;
+  for ($r = 1; $r <= $self->{ _yDim }; $r++) {
+    for ($c = 1; $c <= $self->{ _xDim }; $c++) {
+      $val = $self->{ _matrixArray }[$self->getMatrixIndex($c, $r)];
+      if (!defined $val || $method eq "") {
+        $printVal = $val;
+      } else {
+        $printVal = $val->$method;
+      }
       print " $printVal ";
     }
     print "\n";
   }
+}
+
+sub getMatrixIndex {
+  $self = shift;
+  $x = shift;
+  $y = shift;
+  return $x - 1 + $self->{ _xDim } * ($y - 1);
 }
 
 1;
