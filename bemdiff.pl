2b0d31d3124e2764f7863c2dde451909d78a892f
Brandon Montgomery <bmonty@gmail.com>
2011-02-10 00:10:36 -0500

    cleaning up some commented out debugging code

--- bemdiff.pl
+++ bemdiff.pl
@@ -43,25 +43,6 @@ sub buildHash {
 buildHash($OLDFILE, \%lines, \%map, \@oldFileMap);
 buildHash($NEWFILE, \%lines, \%map, \@newFileMap);
 
-# print out the hashes for debugging
-#for my $key (keys(%lines)) {
-#  print "$key: $lines{$key}\n";
-#}
-
-# print out the hashes for debugging
-#for my $key (keys(%map)) {
-#  print "$key: $map{$key}\n";
-#}
-
-# print out arrays for debugging
-#foreach (@oldFileMap) {
-#  print "$_\n";
-#}
-
-#foreach (@newFileMap) {
-#  print "$_\n";
-#}
-
 # build edit distance matrix
 $colCount = scalar(@oldFileMap) + 1;
 $rowCount = scalar(@newFileMap) + 1;
@@ -71,25 +52,21 @@ $editMatrix = new Matrix($colCount, $rowCount);
 
 # initialize distances for first column
 for ($i = 1; $i <= $rowCount; $i++) { 
-  #print "x: 0, y: $i, dis: $i fromtop\n";
   $editMatrix->setValue(1, $i, new EditMatrixNode($i - 1, "fromtop")); 
 }
 
 # initialize distances for first row
 for ($i = 1; $i <= $colCount; $i++) { 
-  #print "x: $i, y: 0, dis: $i fromleft\n";
   $editMatrix->setValue($i, 1, new EditMatrixNode($i - 1, "fromleft")); 
 }
 
-#$editMatrix->print("getVector");
-
-#now, start at 2,2 and determine the best way to get there
+# now, start at 2,2 and determine the best way to get there, repeat for
+# all vertices in grid 
 for ($x = 2; $x <= $colCount; $x++) {
   for ($y = 2; $y <= $rowCount; $y++) {
     #first, see if it's a diagonal
     if (@oldFileMap[$x - 2] eq @newFileMap[$y - 2]) {
       $editMatrix->setValue($x, $y, new EditMatrixNode($editMatrix->getValue($x - 1, $y - 1)->{ _distance }, "diag"));
-      #print "x: $x, y: $y, dir: diag\n";
     } else {
       #try from the top and from the left, see which is better.
       $distanceFromTop = $editMatrix->getValue($x, $y - 1)->getDistance() + 1;
@@ -97,27 +74,22 @@ for ($x = 2; $x <= $colCount; $x++) {
       if ($distanceFromTop > $distanceFromLeft) {
         #left looks better! we'll take that instead.
         $editMatrix->setValue($x, $y, new EditMatrixNode($distanceFromLeft, "fromleft"));
-        #print "x: $x, y: $y, dir: fromleft; dl: $distanceFromLeft, dt: $distanceFromTop\n";
       } else {
         #top looks good! we'll take it.
         $editMatrix->setValue($x, $y, new EditMatrixNode($distanceFromTop, "fromtop"));
-        #print "x: $x, y: $y, dir: fromtop; dl: $distanceFromLeft, dt: $distanceFromTop\n";
       }
     }
   }
 }
 
-#$editMatrix->print("getVector");
-
-#transform the edit matrix into the minimum list of steps to transform the old string into the new string
-#start at the bottom-right node, and move backwards through the edit matrix, following the recorded directions
+# transform the edit matrix into the minimum list of steps to transform the old string into the new string
+# start at the bottom-right node, and move backwards through the edit matrix, following the recorded directions
 $x = $colCount;
 $y = $rowCount;
 @steps = ();
 while($x != 1 || $y != 1) {
   #record direction - only need to record it if it's not diagonal
   $dir = $editMatrix->getValue($x, $y)->getDirection();
-  #print "x: $x, y: $y; dir: $dir\n";
   unshift(@steps, { 
       _direction => $dir, 
       _indexA => $x - 1, 
