29a47862a5dedab2ec9bf8bcc69ace3a7d8ff1a9
Brandon Montgomery <bmonty@gmail.com>
2011-02-10 00:04:50 -0500

    finish up line diff, clean up debugging prints, swap deletes and inserts in human friendly view

--- bemdiff.pl
+++ bemdiff.pl
@@ -44,23 +44,23 @@ buildHash($OLDFILE, \%lines, \%map, \@oldFileMap);
 buildHash($NEWFILE, \%lines, \%map, \@newFileMap);
 
 # print out the hashes for debugging
-for my $key (keys(%lines)) {
-  print "$key: $lines{$key}\n";
-}
+#for my $key (keys(%lines)) {
+#  print "$key: $lines{$key}\n";
+#}
 
 # print out the hashes for debugging
-for my $key (keys(%map)) {
-  print "$key: $map{$key}\n";
-}
+#for my $key (keys(%map)) {
+#  print "$key: $map{$key}\n";
+#}
 
 # print out arrays for debugging
-foreach (@oldFileMap) {
-  print "$_\n";
-}
+#foreach (@oldFileMap) {
+#  print "$_\n";
+#}
 
-foreach (@newFileMap) {
-  print "$_\n";
-}
+#foreach (@newFileMap) {
+#  print "$_\n";
+#}
 
 # build edit distance matrix
 $colCount = scalar(@oldFileMap) + 1;
@@ -81,7 +81,7 @@ for ($i = 1; $i <= $colCount; $i++) {
   $editMatrix->setValue($i, 1, new EditMatrixNode($i - 1, "fromleft")); 
 }
 
-$editMatrix->print("getVector");
+#$editMatrix->print("getVector");
 
 #now, start at 2,2 and determine the best way to get there
 for ($x = 2; $x <= $colCount; $x++) {
@@ -94,20 +94,20 @@ for ($x = 2; $x <= $colCount; $x++) {
       #try from the top and from the left, see which is better.
       $distanceFromTop = $editMatrix->getValue($x, $y - 1)->getDistance() + 1;
       $distanceFromLeft = $editMatrix->getValue($x - 1, $y)->getDistance() + 1;
-      if ($distanceFromTop < $distanceFromLeft) {
-        #top looks good! we'll take it.
-        $editMatrix->setValue($x, $y, new EditMatrixNode($distanceFromTop, "fromtop"));
-        #print "x: $x, y: $y, dir: fromtop; dl: $distanceFromLeft, dt: $distanceFromTop\n";
-      } else {
+      if ($distanceFromTop > $distanceFromLeft) {
         #left looks better! we'll take that instead.
         $editMatrix->setValue($x, $y, new EditMatrixNode($distanceFromLeft, "fromleft"));
         #print "x: $x, y: $y, dir: fromleft; dl: $distanceFromLeft, dt: $distanceFromTop\n";
+      } else {
+        #top looks good! we'll take it.
+        $editMatrix->setValue($x, $y, new EditMatrixNode($distanceFromTop, "fromtop"));
+        #print "x: $x, y: $y, dir: fromtop; dl: $distanceFromLeft, dt: $distanceFromTop\n";
       }
     }
   }
 }
 
-$editMatrix->print("getVector");
+#$editMatrix->print("getVector");
 
 #transform the edit matrix into the minimum list of steps to transform the old string into the new string
 #start at the bottom-right node, and move backwards through the edit matrix, following the recorded directions
@@ -118,49 +118,42 @@ while($x != 1 || $y != 1) {
   #record direction - only need to record it if it's not diagonal
   $dir = $editMatrix->getValue($x, $y)->getDirection();
   #print "x: $x, y: $y; dir: $dir\n";
-  if (!($dir eq "d")) {
-    unshift(@steps, { 
-        _direction => $dir, 
-        _indexA => $x - 1, 
-        _indexB => $y - 1
-      });
-  }
+  unshift(@steps, { 
+      _direction => $dir, 
+      _indexA => $x - 1, 
+      _indexB => $y - 1
+    });
   $x -= $dir eq "d" || $dir eq "l" ? 1 : 0;
   $y -= $dir eq "d" || $dir eq "t" ? 1 : 0;
 }
 
 #write out a human friendly representation of the change which were made
-#@diffChars = split("", $oldString);
-#@indicator = ();
-#for ($i = 0; $i < length($oldString) + length($newString); $i++) { 
-#  $indicator[$i] = " "; 
-#}
-#
-##print "$oldString\n";
-##print "$newString\n";
-#$offset = 0;
-#$indicatorOffset = 0;
-#foreach (@steps) {
-#  if ($_->{ _direction } eq "l") {
-#    $adjIndexA = $_->{ _indexA } + $offset - 1;
-#    $adjIndexOffset = $adjIndexA + $indicatorOffset;
-#    $charToDel = substr($oldString, $adjIndexA - $offset, 1);
-#    #print "delete char at index $adjIndexA (\"$charToDel\")\n";
-#    $indicator[$adjIndexOffset] = "-";
-#    $offset--;
-#    $indicatorOffset++;
-#  } elsif ($_->{ _direction } eq "t") {
-#    $adjIndexA = $_->{ _indexA } + $offset;
-#    $adjIndexB = $_->{ _indexB } - 1;
-#    $adjIndexOffset = $adjIndexA + $indicatorOffset;
-#    $charToIns = substr($newString, $adjIndexB, 1);
-#    #print "insert char from the new string at index $adjIndexB (\"$charToIns\") into the old string at index $adjIndexA\n";
-#    $offset++;
-#    splice(@diffChars, $adjIndexOffset, 0, $charToIns);
-#    $indicator[$adjIndexOffset] = "+";
-#  }
-#}
-#
-#$diffString = join("", @diffChars);
-#$indicatorString = join("", @indicator);
-#print "$diffString\n$indicatorString\n";
+$indexOld = 0;
+$indexNew = 0;
+@diff = ();
+foreach (@steps) {
+  if ($_->{ _direction } eq "l") {
+    # this is a delete from the old file. in this case, we show the line
+    # from the old file with a "-" in front of it.
+    $indicator = "-";
+    $line = $map{$oldFileMap[$indexOld++]};
+  } elsif ($_->{ _direction } eq "t") {
+    # this is an insert from the new file. in this case, we show the line
+    # from the new file with a "+" in front of it.
+    $indicator = "+";
+    $line = $map{$newFileMap[$indexNew++]};
+  } else {
+    # no change - in this case, we show the line (same in both files) and
+    # increment both file indexes.
+    $indicator = " ";
+    $line = $map{$newFileMap[$indexNew++]};
+    $indexOld++;
+  }
+  # since we chomped the line when reading it into the hash, add a \n at
+  # the end
+  @diff[scalar(@diff)] = "$indicator $line\n";
+}
+
+foreach $line (@diff) {
+  print "$line";
+}
